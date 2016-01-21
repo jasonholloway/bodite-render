5f121def37efc489ecdbc6e51cf90060381c5aed
Jason Holloway <miceeatnicerice@hotmail.com>
2016-01-21 22:08:06 +0000

    Fixed .gitignore

--- render/Program.fs
+++ render/Program.fs
@@ -1,7 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 [<EntryPoint>]
-let main argv = 
+let main argv =     
     printfn "Hello Jason!"
     0 // return an integer exit code
-
+    //pipe products into ...
+    //pipe categories into ...

e1d1c296c901e55ca43920219244819a0080a18d
Jason Holloway <miceeatnicerice@hotmail.com>
2016-01-22 14:24:37 +0000

    Added first NUnit test

---
+++ render.engine.test/EngineTests.fs
@@ -0,0 +1,29 @@
+﻿namespace render.engine.test
+
+open NUnit.Framework
+open FsUnit
+open render.engine
+
+module EngineTests =
+
+    type Hamster = {
+        name:string;
+        colour:string;
+    }
+
+
+    [<Test>]
+    let ``renderString renders with model`` () =        
+        let template = [|
+                            "@using RazorEngine.Templating"
+                            "@using render.engine.test";
+                            "@inherits TemplateBase<EngineTests.Hamster>";
+                            "Hello @Model.name, you are @Model.colour!";
+                        |]
+                        |> String.concat System.Environment.NewLine
+    
+        { name = "Jeremy"; colour = "mauve" }
+        |> Engine.renderString template
+        |> should equal "Hello Jeremy, you are mauve!"
+
+

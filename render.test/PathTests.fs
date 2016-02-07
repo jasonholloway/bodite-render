d4d79d7af9f073a8e7e1eb0121b476428a1f5dc7
Jason Holloway <miceeatnicerice@hotmail.com>
2016-02-07 21:24:45 +0000

    Added path resolution

---
+++ render.test/PathTests.fs
@@ -0,0 +1,49 @@
+﻿module Paths
+
+open System
+open NUnit.Framework
+open FsUnit
+open FSharp.Data
+open Products
+open Categories
+open Paths
+
+
+
+let createCat () =
+    {
+        Key = System.Guid.NewGuid().ToString()
+        Name = { LV = None; RU = None }
+        Description = { LV = None; RU = None }
+        Children = []
+        Products = []
+    }
+
+let createProd () =
+    {
+        Key = System.Guid.NewGuid().ToString()
+        Name = { LV = None; RU = None }
+        Description = { LV = None; RU = None }
+        MachineName = "kjhkjhuhu"
+        CategoryKeys = []
+    }
+    
+
+
+[<TestFixture>]
+type ``resolvePath`` () =
+
+    [<Test>]
+    member x.``resolves Product`` () =
+        let prod = createProd()
+
+        Paths.resolvePath prod
+        |> should equal ("product/" + prod.MachineName)
+
+
+    [<Test>]
+    member x.``resolves Category`` () =
+        let cat = createCat()
+
+        Paths.resolvePath cat
+        |> should equal ("category/" + cat.Key)
---
+++ render/Paths.fs
@@ -0,0 +1,18 @@
+﻿module Paths
+
+open Shared
+open Products
+open Categories
+
+let resolvePath (m: 'M) =
+    match box m with
+    | :? Product ->
+        let p = box m :?> Product
+        "product/" + p.MachineName 
+
+    | :? Category ->
+        let c = box m :?> Category
+        "category/" + c.Key
+
+    | _ -> 
+        failwith "Can't resolve path for mystery object"

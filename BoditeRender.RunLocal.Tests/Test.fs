e399a57ea9acac43c61fea897740da1be331f302
Jason Holloway <miceeatnicerice@hotmail.com>
2016-02-15 16:05:55 +0000

    Added RunLocal.Tests

---
+++ BoditeRender.RunLocal.Tests/Test.fs
@@ -0,0 +1,39 @@
+﻿namespace BoditeRender.RunLocal.Tests
+
+open NUnit.Framework
+open FsUnit
+open BoditeRender
+open System.IO
+
+
+[<TestFixture>]
+type ``getFSResolver`` () =
+    
+    [<Test>]
+    member x.``resolves file from immediate directory`` () =
+        use file = new Helpers.TempFile("file.cshtml", "templatecontents")
+        
+        let resolver = FSResolver.create file.DirPath
+
+        resolver "file.cshtml"
+        |> should equal "templatecontents"
+
+
+
+
+[<TestFixture>]
+type ``getFSCommitter`` () =
+
+    [<Test>]
+    member x.``commits to immediate directory`` () =
+        use folder = new Helpers.TempFolder()
+
+        let committer = FSCommitter.create folder.Path
+                    
+        use vf = new VirtFile("file.html", "blarg")
+                       
+        committer vf 
+        |> ignore
+
+        File.ReadAllText(Path.Combine(folder.Path, vf.Path))
+        |> should equal "blarg"
--- BoditeRender.Tests/RenderTests.fs
+++ BoditeRender.Tests/RenderTests.fs
@@ -72,35 +72,3 @@ type ``renderPage`` () =
         |> should equal "hello!"
 
 
-
-[<TestFixture>]
-type ``getFSResolver`` () =
-    
-    [<Test>]
-    member x.``resolves file from immediate directory`` () =
-        use file = new Helpers.TempFile("file.cshtml", "templatecontents")
-        
-        let resolver = Render.getFSResolver file.DirPath
-
-        resolver "file.cshtml"
-        |> should equal "templatecontents"
-
-
-
-
-[<TestFixture>]
-type ``getFSCommitter`` () =
-
-    [<Test>]
-    member x.``commits to immediate directory`` () =
-        use folder = new Helpers.TempFolder()
-
-        let committer = Render.getFSCommitter folder.Path
-                    
-        use vf = new VirtFile("file.html", "blarg")
-                       
-        committer vf 
-        |> ignore
-
-        File.ReadAllText(Path.Combine(folder.Path, vf.Path))
-        |> should equal "blarg"
